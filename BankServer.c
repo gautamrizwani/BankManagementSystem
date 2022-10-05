6c47519fb856f23c3c7c19caf501699d6676fa58
gautamrizwani <gautamrizwani@gmail.com>
2022-10-05 12:57:32 +0000

    First Commit

---
+++ BankServer.c
@@ -0,0 +1,12 @@
+#include <stdio.
+#include <errno.h> 
+
+#include <fcntl.h>  
+#include <unistd.h>  
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+#include <string.h> 
+#include <stdbool.h>
+#include <stdlib.h>
